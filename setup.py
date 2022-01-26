acce5a364a1db9afd26325036ab483f5d5829e93
RezuanDzibov <rezuan.dzbov@gmail.com>
2022-01-26 18:08:15 +0300

    0.0.3 ver

--- setup.py
+++ setup.py
@@ -5,16 +5,13 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="scraping-mixins",
-    version="0.0.1",
+    version="0.0.3",
     author="Rezuan Dzibov",
     author_email="rezuan.dzbov.work@gmail.com",
     description="",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/RezuanDzibov/scraping_mixins_package",
-    project_urls={
-        "Bug Tracker": "https://github.com/RezuanDzibov/scraping_mixins_package/issues",
-    },
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
